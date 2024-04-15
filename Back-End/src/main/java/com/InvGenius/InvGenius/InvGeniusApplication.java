1d297b4bf1d2353034b376e718b9019ff5db8255
Anyi Casanova <anyi22195@gmail.com>
2024-04-15 13:56:36 -0500

    subir carpetas

---
+++ Back-End/src/main/java/com/InvGenius/InvGenius/InvGeniusApplication.java
@@ -0,0 +1,13 @@
+package com.InvGenius.InvGenius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvGeniusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvGeniusApplication.class, args);
+	}
+
+}
