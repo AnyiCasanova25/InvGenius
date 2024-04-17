7ad208c5ac762c176460ba2f89750f3a1fdaad05
Yordy Nuñez <yordierik05@gmail.com>
2024-04-17 15:54:02 -0500

    Se crearon los models de ubicacion y categoria en Spring

---
+++ Back-End/src/main/java/com/InvGenius/InvGenius/models/categoria.java
@@ -0,0 +1,72 @@
+package com.InvGenius.InvGenius.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity (name = "categoria")
+public class categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID);
+    @Column(name = "idCategoria", nullable = false, length = 36)
+    private String idCategoria;
+
+    @Column(name = "nombreCategoria", nullable = false, length = 20)
+    private String nombreCategoria;
+
+    @Column(name = "estado", nullable = false, length = 20)
+    private String estado;
+
+    @OneToOne
+    @JoinColumn(name = "idUbicacion")
+    private ubicacion ubicacion;
+
+    public categoria() {
+    }
+
+    public categoria(String idCategoria, String nombreCategoria, String estado,
+            com.InvGenius.InvGenius.models.ubicacion ubicacion) {
+        this.idCategoria = idCategoria;
+        this.nombreCategoria = nombreCategoria;
+        this.estado = estado;
+        this.ubicacion = ubicacion;
+    }
+
+    public String getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(String idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombreCategoria() {
+        return nombreCategoria;
+    }
+
+    public void setNombreCategoria(String nombreCategoria) {
+        this.nombreCategoria = nombreCategoria;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public ubicacion getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(ubicacion ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+}
---
+++ Back-End/src/main/java/com/InvGenius/InvGenius/models/ubicacion.java
@@ -0,0 +1,59 @@
+package com.InvGenius.InvGenius.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity(name = "ubicacion")
+public class ubicacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID);
+    @Column(name = "idUbicacion", nullable = false, length = 36)
+    private String idUbicacion;
+
+    @Column(name = "asignarUbicacion", nullable = false, length = 20)
+    private String asignarUbicacion;
+
+    @Column(name = "listarProductoEnUbicacion", nullable = false, length = 30)
+    private String listarProductoEnUbicacion;
+
+    public ubicacion() {
+    }
+
+    public ubicacion(String idUbicacion, String asignarUbicacion, String listarProductoEnUbicacion) {
+        this.idUbicacion = idUbicacion;
+        this.asignarUbicacion = asignarUbicacion;
+        this.listarProductoEnUbicacion = listarProductoEnUbicacion;
+    }
+
+    public String getIdUbicacion() {
+        return idUbicacion;
+    }
+
+    public void setIdUbicacion(String idUbicacion) {
+        this.idUbicacion = idUbicacion;
+    }
+
+    public String getAsignarUbicacion() {
+        return asignarUbicacion;
+    }
+
+    public void setAsignarUbicacion(String asignarUbicacion) {
+        this.asignarUbicacion = asignarUbicacion;
+    }
+
+    public String getListarProductoEnUbicacion() {
+        return listarProductoEnUbicacion;
+    }
+
+    public void setListarProductoEnUbicacion(String listarProductoEnUbicacion) {
+        this.listarProductoEnUbicacion = listarProductoEnUbicacion;
+    }
+
+
+}
