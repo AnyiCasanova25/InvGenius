1efa8b93cc62449e217c157862067c92d663cbde
unknown <SENA@NEIAPRSINFSD610.FormacionSena.RED>
2024-04-17 17:48:26 -0500

    Editar controller de user

--- Back-End/src/main/java/com/InvGenius/InvGenius/Controller/userController.java
+++ Back-End/src/main/java/com/InvGenius/InvGenius/Controller/userController.java
@@ -1,14 +1,47 @@
 package com.InvGenius.InvGenius.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.InvGenius.InvGenius.interfaceService.IuserService;
+import com.InvGenius.InvGenius.models.user;
 
 @RestController
 @RequestMapping("/api/v1/user")
 
 public class userController {
     
+    //  @Autowired
+    //  private IuserService userService;
+
+    // @PostMapping("/")
+    // public ResponseEntity<Object> save (@ModelAttribute("user") user user){
+
+    //     //Verificar que no exista numero de telefono 
+    //  var listaUser = userService.findAll()
+    // .stream().filter(user -> user.getCelular()
+    //     .equals(user.getCelular()));
+
+    // if (listaUser.count() !=0) {
+    //         return new ResponseEntity<>("El numero de celular ya existe", HttpStatus.BAD_REQUEST);
+    //     }
+
+    //     listaUser = userService.findAll()
+    //     .stream().filter(user ->user.getCorreo()
+    //     .equals(user.getCorreo()));
+
+    //     if (listaUser.count()!=0) {
+    //         return new ResponseEntity<>("El correo ya existe", HttpStatus.BAD_REQUEST); 
+    //     }
+
+
+    // }
     @GetMapping("/")
     public String home() {
 
