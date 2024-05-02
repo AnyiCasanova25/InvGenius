50139e211f5fc46dfe3abcd374d4a66889beecb7
Yordy Nuñez <yordierik05@gmail.com>
2024-05-02 16:35:57 -0500

    modifique filtro de lote

--- Back-End/src/main/java/com/InvGenius/InvGenius/interfaces/Ilote.java
+++ Back-End/src/main/java/com/InvGenius/InvGenius/interfaces/Ilote.java
@@ -10,7 +10,7 @@ import com.InvGenius.InvGenius.models.lote;
 
 @Repository
 public interface Ilote extends CrudRepository<lote, String> {
-    @Query ("SELECT l FROM lote l WHERE l.fehcaingreso = ?1 OR l.fechaVencimiento = ?2")
+    @Query ("SELECT l FROM lote l WHERE l.fechaingreso = ?1 OR l.fechaVencimiento = ?2")
     List<lote> loteExist(Date fechaIngreso, Date fechaVencimiento);
     
     
