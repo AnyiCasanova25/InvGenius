d46801b62eb9f59d833152b7218f65ab6f07450a
Anyi Casanova <anyi22195@gmail.com>
2024-05-02 14:22:27 -0500

    Se corrigio los warnings

--- Back-End/src/main/java/com/InvGenius/InvGenius/interfaceService/IproductoService.java
+++ Back-End/src/main/java/com/InvGenius/InvGenius/interfaceService/IproductoService.java
@@ -6,17 +6,15 @@ import java.util.Optional;
 import com.InvGenius.InvGenius.models.producto;
 
 public interface IproductoService {
-    
+
     public String save(producto producto);
 
     public List<producto> findAll();
 
-    public List<producto> productoExist(String nombreProducto, String nombreMarca, String nombreCategoria );
+    public List<producto> productoExist(String nombreProducto, String nombreMarca, String nombreCategoria);
 
     public Optional<producto> findOne(String id);
 
-     //cambiar el int
-     public int delete(String id);
-
-     //FALTA EL SERVICIO Y EL CONTROLLER
+    // cambiar el int
+    public int delete(String id);
 }
