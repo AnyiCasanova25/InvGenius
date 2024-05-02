31be1980bd4119e39f1879d4f8165c9c0f48712b
Laura Ariza <arizaalejolauravalentina@gmail.com>
2024-05-02 15:42:45 -0500

    actualizacion de datos

--- Back-End/src/main/java/com/InvGenius/InvGenius/interfaceService/ImovimientoService.java
+++ Back-End/src/main/java/com/InvGenius/InvGenius/interfaceService/ImovimientoService.java
@@ -1,7 +1,8 @@
 package com.InvGenius.InvGenius.interfaceService;
 
 
-import java.sql.Date;
+
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
--- Back-End/src/main/java/com/InvGenius/InvGenius/service/movimientosService.java
+++ Back-End/src/main/java/com/InvGenius/InvGenius/service/movimientosService.java
@@ -1,7 +1,7 @@
 package com.InvGenius.InvGenius.service;
 
 
-import java.sql.Date;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
