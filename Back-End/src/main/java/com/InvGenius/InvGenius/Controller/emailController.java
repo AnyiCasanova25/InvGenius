cbdd3525ca98d106af60b3750123eeee1cfc2450
AnyiCasanova <anyi22195@gmail.com>
2024-06-06 10:29:46 -0500

    Se finalizó el codigo de email, (Falta agregarle la url de las imagenes, y logos de InvGenius)

--- Back-End/src/main/java/com/InvGenius/InvGenius/Controller/emailController.java
+++ Back-End/src/main/java/com/InvGenius/InvGenius/Controller/emailController.java
@@ -1,8 +1,13 @@
 package com.InvGenius.InvGenius.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 @RestController
 public class emailController {
@@ -46,6 +51,97 @@ public class emailController {
     public String enviarCorreoRecuperar() {
         try {
             String destinatario = "dc8987835@gmail.com";
+            String asunto="Recuperacion de contraseña";
+            String cuerpo=""
+            +"<h1>Estimado Usuario</h1>"
+            +"<p>Hemos Recibido su solicitud para restablecer la contraseña de su cuenta en \"InvGenius\".Aqui están los pasos para completar el proceso: </p>\r\n"
+            +"      <ol>\r\n"
+            +"          <li>Haga clic en el siguiente enlace para restablecer su contraseña: [invgenius2024@gmail.com]</li>\r\n"
+            +"      </ol>\r\n"
+            +"<img src'' width'100px' heght='100'>"
+            +"      <p>Por motivos de seguridad, este enlace expirará en [Número de Horas] horas. Si no ha solicitado esta acción, le recomendamos que ignore este correo electronico.</p>\r\n"
+            +"      <p>Si experimenta algún problema o tiene alguna pregunta, no dude en comunicarse con nuestro equipo de soporte. Estamos aqui para ayudarle.</p>\r\n"
+            +"      <p>Atentamente,<br>[Yordy Erik Núñez Pineda]<br>[Genius Inventory Company]<br>[invgenius2024@gmail.com]</p>\r\n";
+
+            var retorno=enviarCorreo(destinatario,asunto,cuerpo);
+            if (retorno) {
+                return "Se envió correctamente";
+            }else{
+                return "No se pudo envíar";
+            }
+        }catch (Exception e) {
+            return "Error al envíar" +e.getMessage();
         }
     }
+
+    @GetMapping("/enviar-correo-cambio")
+    public String enviarCorreoCambio() {
+        try{
+            String destinatario="dc8987835@gmsil.com";
+            String asunto ="Cambio de contraseña";
+            String cuerpo=""
+                    +"<p>Este correo electrónico es para informarle que la contraseña de su cuenta en \"InvGenius\" ha sido cambiada con éxito. Si usted realizó este cambio, puede ignorar este mensaje. En caso contrario, le recomendamos que se comunique con nuestro equipo de soporte de inmediato.</p>\r\n "
+                    +"<img src='' width='100px' heght='100px'>"
+                    +"      <p>Si tiene alguna pregunta o necesita asistencia, no dude en ponerse en contacto con nosotros. Estamos aquí para ayudarle. </p>\r\n"
+                    +"      <p>Atentamente,<br>[Julian David Fierro Casanova]<br>[Genius Inventory Company]<br>[invgenius2024@gmail.com]</p>\r\n";
+
+                    var retorno= enviarCorreo(destinatario,asunto,cuerpo);
+                    if (retorno) {
+                        return "Se envió correctamente";
+                    }else{
+                        return "No se pudo envíar";
+                    }
+        }catch (Exception e){
+            return "Error al envíar"+e.getMessage();
+        }
+    }
+
+
+    @GetMapping("/enviar-correo-caducar")
+    public String enviarCorreoCaducar() {
+        try {
+            String destinatario = "dc8987835@gmail.com";
+            String asunto = "Producto Próximo a Caducar";
+            String cuerpo = ""
+                    + "<h1>Estimado Usuario</h1>"
+                    + "<p>Le informamos que uno de los productos en su inventario registrado en <strong>InvGenius</strong> está próximo a caducar. A continuación, se detalla la información del producto:</p>\r\n"
+                    + "<ul>\r\n"
+                    + "    <li><strong>Producto:</strong> Leche Entera</li>\r\n"
+                    + "    <li><strong>Marca:</strong> Alpina</li>\r\n"
+                    + "    <li><strong>Cantidad:</strong> 5 unidades</li>\r\n"
+                    + "    <li><strong>Fecha de Caducidad:</strong> 15 de junio de 2024</li>\r\n"
+                    + "</ul>\r\n"
+                    + "<p>Le recomendamos que venda o promocione este producto antes de la fecha de caducidad indicada para asegurar su frescura y evitar desperdicios.</p>\r\n"
+                    + "<img src='https://example.com/images/leche.png' width='100px' height='100px'>"
+                    + "<p>Si tiene alguna pregunta o necesita asistencia adicional, no dude en ponerse en contacto con nuestro equipo.</p>\r\n"
+                    + "<p>Atentamente,<br>[Anyi Zujey Gomez Casanova]<br>[Genius Inventory Company]<br>[invgenius2024@gmail.com]</p>\r\n";
+    
+            var retorno = enviarCorreo(destinatario, asunto, cuerpo);
+            if (retorno) {
+                return "Se envió correctamente";
+            } else {
+                return "No se pudo enviar";
+            }
+    
+        } catch (Exception e) {
+            return "Error al enviar: " + e.getMessage();
+        }
+    }
+
+    public boolean enviarCorreo(String destinatario, String asunto, String cuerpo) throws MessagingException {
+        try{
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+            helper.setTo(destinatario);
+            helper.setSubject(asunto);
+            helper.setText(cuerpo, true);
+
+            javaMailSender.send(message);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    
 }
