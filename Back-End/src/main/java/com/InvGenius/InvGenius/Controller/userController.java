09086cab8720cbc4588f74510f9ce3ed0965bbfb
AnyiCasanova <anyi22195@gmail.com>
2024-07-11 15:46:20 -0500

    prueba de funcionamiento de la API

--- Back-End/src/main/java/com/InvGenius/InvGenius/Controller/userController.java
+++ Back-End/src/main/java/com/InvGenius/InvGenius/Controller/userController.java
@@ -20,7 +20,6 @@ import com.InvGenius.InvGenius.models.user;
 import lombok.var;
 
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1/user")
