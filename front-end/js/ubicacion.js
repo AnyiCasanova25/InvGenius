71342afc3522b7178a7e95186906d2e42ef8e79c
Yordy Nuñez <yordierik05@gmail.com>
2024-07-18 08:52:59 -0500

    Se hizo parte del js de ubicacion

---
+++ front-end/js/ubicacion.js
@@ -0,0 +1,34 @@
+url: "http://localhost:8080/api/v1/ubicacion"
+
+function registrarUbicacion(){
+    let asignar_ubicacion = document.getElementById("asignarUbicacion").value;
+    let bloques = document.getElementById("bloques").value;
+
+    //Datos del formulario de ubicacion
+    let formData = {
+        "asignar_ubicacion": asignar_ubicacion,
+        "bloques": bloques
+    };
+
+    $.ajax({
+        url: url,
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(formData),
+        success: function (result){
+            Swal.fire({
+                title: "¡Excelente!",
+                text: "Se guardó correctamente",
+                icon: "success"
+            });
+            limpiarFormulario();
+        },
+        error:function (error){
+            Swal.fire({
+                title: "Error",
+                text: "Error al guardar el libro",
+                icon: "error"
+            });
+        }
+    });
+}
