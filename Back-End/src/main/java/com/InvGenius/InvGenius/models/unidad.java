aac1237514e9f73024c1d18971cf8aeba4b2c8f6
Anyi Zujey <anyi22195@gmail.com>
2024-08-14 16:58:50 -0500

    se elimino el atributo tipo de unidad en la entidad unidad

--- Back-End/src/main/java/com/InvGenius/InvGenius/models/unidad.java
+++ Back-End/src/main/java/com/InvGenius/InvGenius/models/unidad.java
@@ -22,9 +22,6 @@ public class unidad {
      @Column(name = "nombreUnidad", nullable = false, length = 36)
      private String nombreUnidad;
 
-     @Column(name = "tipoUnidad",nullable = false,length = 36)
-     private String tipoUnidad;
-
      @Column(name = "estadoUnidad", nullable = false, length = 20)
      private String estadoUnidad;
 
@@ -35,19 +32,9 @@ public class unidad {
     public unidad(String idUnidad, String nombreUnidad, String tipoUnidad, String estadoUnidad) {
         this.idUnidad = idUnidad;
         this.nombreUnidad = nombreUnidad;
-        this.tipoUnidad = tipoUnidad;
         this.estadoUnidad = estadoUnidad;
     }
 
-
-    public String getTipoUnidad() {
-        return tipoUnidad;
-    }
-
-
-    public void setTipoUnidad(String tipoUnidad) {
-        this.tipoUnidad = tipoUnidad;
-    }
 
 
     public String getIdUnidad() {
