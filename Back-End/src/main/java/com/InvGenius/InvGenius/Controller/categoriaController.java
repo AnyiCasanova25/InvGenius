19f46afacea212d02665c7a2a74e5fcb3c6c707e
Yordy Nuñez <yordierik05@gmail.com>
2024-09-11 14:06:30 -0500

    Se elimino el lombok.var del controller de categoria

--- Back-End/src/main/java/com/InvGenius/InvGenius/Controller/categoriaController.java
+++ Back-End/src/main/java/com/InvGenius/InvGenius/Controller/categoriaController.java
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import lombok.var;
 
 @RestController
 @RequestMapping("/api/v1/categoria")
