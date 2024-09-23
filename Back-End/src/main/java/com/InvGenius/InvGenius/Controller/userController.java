f37a3d104571eefd00ee535f932fd3d7fb031a0e
Anyi Zujey <anyi22195@gmail.com>
2024-09-23 16:03:02 -0500

    validacion de password

--- Back-End/src/main/java/com/InvGenius/InvGenius/Controller/userController.java
+++ Back-End/src/main/java/com/InvGenius/InvGenius/Controller/userController.java
@@ -1,11 +1,11 @@
 package com.InvGenius.InvGenius.Controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.InvGenius.InvGenius.interfaceService.IuserService;
+import com.InvGenius.InvGenius.models.changePasswordRequest;
 import com.InvGenius.InvGenius.models.rol;
 import com.InvGenius.InvGenius.models.user;
 
@@ -26,27 +27,32 @@ public class userController {
     @Autowired
     private IuserService userService;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     // @Autowired
     // private JavaMailSender javaMailSender;
 
     // private static int numeroAleatorioEnRango(int minimo, int maximo) {
-    //     // nextInt regresa en rango pero con límite superior exclusivo, por eso sumamos
-    //     // 1
-    //     return ThreadLocalRandom.current().nextInt(minimo, maximo + 1);
+    // // nextInt regresa en rango pero con límite superior exclusivo, por eso
+    // sumamos
+    // // 1
+    // return ThreadLocalRandom.current().nextInt(minimo, maximo + 1);
     // }
 
     // private String codigoAleatorio() {
-    //     int longitud = 6;
-    //     // El banco de caracteres
-    //     String banco = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-    //     // La cadena en donde iremos agregando un carácter aleatorio
-    //     String cadena = "";
-    //     for (int x = 0; x < longitud; x++) {
-    //         int indiceAleatorio = numeroAleatorioEnRango(0, banco.length() - 1);
-    //         char caracterAleatorio = banco.charAt(indiceAleatorio);
-    //         cadena += caracterAleatorio;
-    //     }
-    //     return cadena;
+    // int longitud = 6;
+    // // El banco de caracteres
+    // String banco =
+    // "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    // // La cadena en donde iremos agregando un carácter aleatorio
+    // String cadena = "";
+    // for (int x = 0; x < longitud; x++) {
+    // int indiceAleatorio = numeroAleatorioEnRango(0, banco.length() - 1);
+    // char caracterAleatorio = banco.charAt(indiceAleatorio);
+    // cadena += caracterAleatorio;
+    // }
+    // return cadena;
     // }
 
     @PostMapping("/register/")
@@ -89,19 +95,6 @@ public class userController {
 
         // user.setPassword(codigoAleatorio());
 
-        
-
-        // if (user.getPassword().equals("")) {
-
-        // return new ResponseEntity<>("La contraseña es un campo obligatorio",
-        // HttpStatus.BAD_REQUEST);
-        // }
-
-        // if (user.getConfirmarPassword().equals("")) {
-
-        //     return new ResponseEntity<>("Confirme su contraseña correctamente", HttpStatus.BAD_REQUEST);
-        // }
-
         // todo bien
         user.setRol(rol.User);
         userService.save(user);
@@ -110,39 +103,60 @@ public class userController {
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-
     @GetMapping("profile/")
     public ResponseEntity<user> getProfile() {
-       Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-       user user = (user) auth.getPrincipal();
-       return new ResponseEntity<user>(user, HttpStatus.OK);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        user user = (user) auth.getPrincipal();
+        return new ResponseEntity<user>(user, HttpStatus.OK);
+    }
+
+    @PutMapping("/changePassword")
+    public ResponseEntity<?> changePassword(@RequestBody changePasswordRequest request) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        user user = (user) auth.getPrincipal();
+
+        if (user.isCambiarPassword()) {
+            // Validar que las contraseñas coincidan
+            if (!request.getNewPassword().equals(request.getConfirmPassword())) {
+                return new ResponseEntity<>("Las contraseñas no coinciden", HttpStatus.BAD_REQUEST);
+            }
+
+            // Actualizar la contraseña
+            user.setPassword(passwordEncoder.encode(request.getNewPassword()));
+            user.setCambiarPassword(false); // Cambiar el estado de la contraseña a no temporal
+            userService.save(user);
+
+            return new ResponseEntity<>("Contraseña cambiada exitosamente", HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>("La contraseña no es temporal", HttpStatus.BAD_REQUEST);
     }
 
     // @PostMapping("/register/")
     // public ResponseEntity<String> register(@RequestBody String entity) {
-    //     return new ResponseEntity<>("end-point Publico register",HttpStatus.OK);
+    // return new ResponseEntity<>("end-point Publico register",HttpStatus.OK);
     // }
 
     @GetMapping("/admin/findAll/")
-    public ResponseEntity <String> findAll() {
+    public ResponseEntity<String> findAll() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         var user = (user) auth.getPrincipal();
         if (user.getRol() != rol.Admin)
-          return new ResponseEntity<String>("No tiene permiso",HttpStatus.FORBIDDEN);
-        return new ResponseEntity<String>("Metodo admin", HttpStatus.OK);    
+            return new ResponseEntity<String>("No tiene permiso", HttpStatus.FORBIDDEN);
+        return new ResponseEntity<String>("Metodo admin", HttpStatus.OK);
     }
-    
+
     // @GetMapping("/")
     // public ResponseEntity<Object> findAll(){
-    //     var listaUser = userService.findAll();
-    //     return new ResponseEntity<>(listaUser,HttpStatus.OK);
+    // var listaUser = userService.findAll();
+    // return new ResponseEntity<>(listaUser,HttpStatus.OK);
     // }
 
-   @GetMapping("/busquedaFiltros/{filtro}")
-   public ResponseEntity<Object> findFiltro(@PathVariable String filtro) {
-       var listaUser = userService.userExist(filtro,filtro);
-       return new ResponseEntity<>(listaUser, HttpStatus.OK);
-   }
+    @GetMapping("/busquedaFiltros/{filtro}")
+    public ResponseEntity<Object> findFiltro(@PathVariable String filtro) {
+        var listaUser = userService.userExist(filtro, filtro);
+        return new ResponseEntity<>(listaUser, HttpStatus.OK);
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<Object> findOne(@PathVariable String id) {
---
+++ Back-End/src/main/java/com/InvGenius/InvGenius/models/changePasswordRequest.java
@@ -0,0 +1,17 @@
+package com.InvGenius.InvGenius.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class changePasswordRequest {
+    
+    private String newPassword;
+    private String confirmPassword;
+}
