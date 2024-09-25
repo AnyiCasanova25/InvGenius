f78cb42d235ceb0b8678da80af20539e314e3187
Anyi Zujey <anyi22195@gmail.com>
2024-09-25 14:49:29 -0500

    comente lineas de codigo

--- Back-End/src/main/java/com/InvGenius/InvGenius/Controller/userController.java
+++ Back-End/src/main/java/com/InvGenius/InvGenius/Controller/userController.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.InvGenius.InvGenius.interfaceService.IuserService;
-import com.InvGenius.InvGenius.models.changePasswordRequest;
+// import com.InvGenius.InvGenius.models.changePasswordRequest;
 import com.InvGenius.InvGenius.models.rol;
 import com.InvGenius.InvGenius.models.user;
 
