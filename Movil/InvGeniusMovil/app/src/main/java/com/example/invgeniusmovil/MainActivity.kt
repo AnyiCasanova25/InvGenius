dd2641338bfe873f8a58e8f3876d009b34d6f615
Willian Gonzalez <willianstebangonzalezcortes@gmail.com>
2024-09-30 22:25:02 -0500

    android cambiado

--- Movil/InvGeniusMovil/app/src/main/java/com/example/invgeniusmovil/MainActivity.kt
+++ Movil/InvGeniusMovil/app/src/main/java/com/example/invgeniusmovil/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.invgeniusmovil
 
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
@@ -11,21 +10,12 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.invgeniusmovil.databinding.ActivityMainBinding
-import com.example.invgeniusmovil.models.Novedades.User
 import com.google.android.material.navigation.NavigationView
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-
-    // Retrofit setup
-    private lateinit var apiService: ApiService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,12 +40,6 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
-        // Initialize Retrofit
-        initRetrofit()
-
-        // Fetch data using Retrofit
-        fetchUsers()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -66,40 +50,5 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
-
-    // Initialize Retrofit
-    private fun initRetrofit() {
-        val retrofit = Retrofit.Builder()
-            .baseUrl("http://localhost:8080/")  // Ajusta la URL de la API
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        // Crear instancia de ApiService
-        apiService = retrofit.create(ApiService::class.java)
-    }
-
-    // Fetch users data from the API
-    private fun fetchUsers() {
-        val call = apiService.getUsers()
-
-        // Ejecutar la llamada de Retrofit
-        call.enqueue(object : Callback<List<User>> {
-            override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
-                if (response.isSuccessful) {
-                    val users = response.body()
-                    // Aquí puedes procesar los usuarios obtenidos
-                    users?.forEach {
-                        Log.d("MainActivity", "User: ${it.nombre} ${it.apellido} ${it.documentos} ${it.correo} ${it.celular} ${it.contraseña} ${it.confirmar_contraseña} ${it.rol}")
-                    }
-                } else {
-                    Log.e("MainActivity", "Error en la respuesta: ${response.errorBody()}")
-                }
-            }
-
-            override fun onFailure(call: Call<List<User>>, t: Throwable) {
-                Log.e("MainActivity", "Error en la llamada a la API", t)
-            }
-        })
     }
 }
---
+++ Movil/InvGeniusMovil/app/src/main/java/com/example/invgeniusmovil/models/Novedades/user.kt
@@ -0,0 +1,8 @@
+package com.example.invgeniusmovil.models.Novedades
+
+
+data class user(
+    val num: String,
+    val name:String
+
+)
