2a6c91bb0dabc11dd619b9befba8ff4cd927a6b0
FerroJuls <julifierrocasanova@gmail.com>
2024-10-08 12:14:20 -0500

    cambio cors

--- Back-End/src/main/java/com/InvGenius/InvGenius/config/corsConfig.java
+++ Back-End/src/main/java/com/InvGenius/InvGenius/config/corsConfig.java
@@ -16,7 +16,8 @@ public class corsConfig {
 
         //permitir solicitudes desde todos los origenes
         //  config.addAllowedOrigin("*");
-                config.addAllowedOrigin("http://127.0.0.1:5502");
+        config.addAllowedOrigin("http://invgenius.com");
+                // config.addAllowedOrigin("http://127.0.0.1:5502");
                 // config.addAllowedOrigin("http://10.192.80.141:5502");
         // config.addAllowedOrigin("http://127.0.0.1:5501/");
 
