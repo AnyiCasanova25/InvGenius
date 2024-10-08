5206a5fb0f35b36d1f017a188048967de04a22a1
FerroJuls <julifierrocasanova@gmail.com>
2024-10-07 20:11:57 -0500

    cambiar url

--- front-end/js/jsUrl/Urls.js
+++ front-end/js/jsUrl/Urls.js
@@ -1,5 +1,5 @@
 //var Servidor = "http://127.0.0.1:5502/api/v1/"
-var Servidor = "http://localhost:8080/api/v1/"
+var Servidor = "http://82.112.245.162:8080/api/v1/"
 var urlMarca = Servidor + "marca/";
 var urlNovedad = Servidor + "novedad/";
 var urlUsuarios = Servidor + "user/";
